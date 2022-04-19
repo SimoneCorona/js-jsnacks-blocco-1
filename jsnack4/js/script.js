90ae77f78fd75e3b5f160762cff604dfb854cdbe
SimoneCorona <corona.simone93@gmail.com>
2022-04-19 16:22:47 +0200

    Finish all snacks

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -7,13 +7,10 @@ do {
     userNum = prompt("Dimmi un numero a 4 cifre");
 } while (userNum.length != 4);
 
-// Creare array del num utente "smontato"
-let arrOfuserNum = Array.from(String(userNum));
-console.log(arrOfuserNum);
-
 // calcola la somma di tutte le cifre che compongono il numero
 let somma = 0;
-for (let i = 0; i <= arrOfuserNum.length; i++) {
-    somma = somma + i; 
+for (let i = 0; i < userNum.length; i++) {
+    console.log('Sommo '+userNum[i]);
+    somma += parseInt(userNum[i]);
 }
-console.log (somma);
+console.log(somma);
